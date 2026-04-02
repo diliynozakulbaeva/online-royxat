c98b9e2631e432bf99ae004b214999d798ff8d43
diliynozakulbaeva <dilnozakulbaeva82@gmail.com>
2026-04-02 09:43:51 +0500

    fgh

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
+import os
 from app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Render PORT muhit o'zgaruvchisidan foydalanadi, u yo'q bo'lsa 5000 ni oladi
+    port = int(os.environ.get("PORT", 5000))
+    # host='0.0.0.0' saytni tashqaridan kirishga ochib beradi
+    app.run(host='0.0.0.0', port=port)
